34590f5a1361cb1f8d930cb8136cca1c97a0c689
HorusWolf <josuemejiafs17@gmail.com>
2025-05-06 20:27:10 -0600

    feat: add services component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/NavBar";
+import Servicios from "@/components/Servicios";
 import { Hero } from "@/components/Hero"
 
 export default function Home() {
 	return (
 		<div className="">
 			<Navbar/>
-			<Hero/>			
+			<Hero/>	
+			<Servicios/>	
 		</div>
 	);
 }
